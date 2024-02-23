64c14b80de1c7769ebda2a53c9346db48f99e432
Manuel Sanchez del Rio <srio@esrf.eu>
2024-02-23 11:30:42 +0100

    added fit size and div profiles

---
+++ scripts/fit-1dProfiles/undulator_divergence.py
@@ -0,0 +1,52 @@
+import numpy
+
+from silx.math.fit.functions import sum_gauss, sum_agauss
+from silx.math.fit import fittheories
+from silx.math.fit.fitmanager import FitManager
+
+
+x = numpy.linspace(-4, 4, 1000)
+Gamma = numpy.pi / 2 * x**2
+y = (numpy.sin(Gamma) / Gamma)**2
+
+
+# Fitting
+p = [y.max(), 0, 2]
+fit = FitManager()
+fit.setdata(x=x, y=y)
+fit.loadtheories(fittheories)
+fit.settheory('Gaussians')
+# fit.settheory('Area Gaussians')
+# fit.settheory('Slit')
+fit.estimate()
+fit.runfit()
+
+print("Searched parameters = %s" % p)
+print("Obtained parameters : ")
+dummy_list = []
+for param in fit.fit_results:
+    # print(param)
+    print(param['name'], ' = ', param['fitresult'])
+    if param['name'] == "FWHM1":
+        print("sigma1 = ", param['fitresult'] / (2 * numpy.sqrt(2 * numpy.log(2))))
+    dummy_list.append(param['fitresult'])
+print("chisq = ", fit.chisq)
+
+y1 = sum_gauss(x, *dummy_list)
+# y1 = sum_agauss(x, *dummy_list)
+
+print("Area original: ", y.sum() * (x[1] - x[0]))
+print("Area fit: ", y1.sum() * (x[1] - x[0]))
+
+
+# plot and save file
+from srxraylib.plot.gol import plot
+p = plot(x, y, x, y1,
+         xtitle=r'$\theta_r=\theta\sqrt{L/\lambda}$', ytitle=r'$(sin(\Gamma)/\Gamma)^2$')
+
+import matplotlib.pylab as plt
+plt.savefig('undulator_divergence.eps')
+
+
+
+
